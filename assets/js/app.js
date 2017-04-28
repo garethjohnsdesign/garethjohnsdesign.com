85f550e970331f106d931513c545f259811bb844
Gareth Johns <info@garethjohnsdesign.com>
2017-04-28 18:32:01 +0100

    Initial Commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,15 @@
 $(document).foundation();
 
 $('body').removeClass('fade-out');
+		
+$('a').mouseenter(function(){
+	$('body').addClass('index--light');
+}).mouseleave(function(){
+	$('body').removeClass('index--light');
+});
 
-$('a[href*="#"]:not([href="#"])').click(function() {
-if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-  var target = $(this.hash);
-  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-  if (target.length) {
-    $('html, body').animate({
-      scrollTop: target.offset().top
-    }, 1000);
-    return false;
-  }
-}
+$('a').click(function(){
+	$('body').addClass('index--light');
+}).mouseleave(function(){
+	$('body').removeClass('index--light');
 });
